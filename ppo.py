4a819c2c6fae3604939df835d852ce88ace3578e
ShonBC <cortesshon@gmail.com>
2022-04-07 18:45:27 -0700

    init

---
+++ ppo.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+import torch as T
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.categorical import Categorical
+
+class PPOMemory:
+    def __init__(self, batch_size):
+        self.states = []
+        self.probs = []
+        self.vals = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
+
+        self.batch_size = batch_size
+
+class ActorNetwok(nn.Module):
+    pass
+
+class CriticNetwork(nn.Module):
+    pass
+
+class Agent():
+    pass
+
