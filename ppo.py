bcbcd28834f4b2f025c3b367f875ccc6a271417e
ShonBC <cortesshon@gmail.com>
2022-04-11 22:07:30 -0700

    Init Actor network

--- ppo.py
+++ ppo.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.distributions.categorical import Categorical
 
+
 class PPOMemory:
     def __init__(self, batch_size):
         self.states = []
@@ -16,12 +17,46 @@ class PPOMemory:
 
         self.batch_size = batch_size
 
+
 class ActorNetwok(nn.Module):
-    pass
+    def __init__(self, n_actions, input_dims, alpha,
+                 fc1_dims=256, fc2_dims=256, chkpt_dir='tmp/ppo'):
+        super().__init__()
+
+        self.checkpoint_file = os.path.join(chkpt_dir, 'actor_torch_ppo')
+        self.actor = nn.Sequential(
+            nn.Linear(*input_dims, fc1_dims),
+            nn.ReLU(),
+            nn.Linear(fc1_dims, fc2_dims),
+            nn.ReLU(),
+            nn.Linear(fc2_dims, n_actions),
+            nn.Softmax(dim=-1)
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        dist = self.actor(state)
+        dist = Categorical(dist)
+
+        return dist
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_checkpoint(T.load(self.checkpoint_file))
+
 
 class CriticNetwork(nn.Module):
     pass
 
-class Agent():
-    pass
 
+class Agent():
+
+    def learn_batch(self, batch, state_arr, actions, old_probs, values,
+                    advantage):
+        
+        states = T.tensor(state_arr[batch], dtype=T.float).to(self.actor.device)
