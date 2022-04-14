901fc55c6400b7a92e40fb64c135d24bb1f3dfbc
ShonBC <cortesshon@gmail.com>
2022-04-13 19:52:07 -0700

    init

---
+++ src/compare_stable_baselines.py
@@ -0,0 +1,27 @@
+import gym
+from stable_baselines3 import PPO, A2C
+
+env = gym.make('HalfCheetah-v3')
+
+env.reset()
+
+mod = int(input('Select Model: \n 1 - PPO \n 2 - A2C'))
+
+if mod == 1:
+    model = A2C('MlpPolicy', env, verbose=1)
+elif mod == 2:
+    model = PPO('MlpPolicy', env, verbose=1)
+
+model.learn(total_timesteps=10000)
+
+episodes = 10
+
+for ep in range(episodes):
+    obs = env.reset()
+    done = False
+    while not done:
+
+        env.render()
+        obs, rewards, done, info = env.step(env.action_space.sample())
+
+env.close()
